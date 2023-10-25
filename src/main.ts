271ce32e313bec7002e83c4eb4fece7726782ef5
Igor-Mont <igornathandev@gmail.com>
2023-10-25 16:35:58 -0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// add security group on EC2 to listen this PORT
+const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 
 bootstrap();
